5cf82e2223548ed17e431c9970bc8e74683a7dab
Satria Aji Nursahid <sajinursahid@gmail.com>
2026-05-31 13:46:41 +0700

    Membuat design dashboard

---
+++ src/main/java/com/mycompany/perpustakaan/Koneksi.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.perpustakaan;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author sajin
+ */
+class Koneksi {
+    
+    static void close() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    private Connection koneksi;
+    public Connection connect(){
+    try{
+        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("Berhasil Koneksi");
+    } catch (ClassNotFoundException ex){
+        System.out.println("Gagal Koneksi"+ex);
+    }
+    String url = "jdbc:mysql://localhost:3306/perpustakaan";
+    try{
+        koneksi = DriverManager.getConnection(url, "root", "");
+        System.out.println("Berhasil Koneksi Database");
+    } catch (SQLException ex){
+        System.out.println("Berhasil Koneksi Database"+ex);
+    }
+    return koneksi;
+    }   
+}
